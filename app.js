be4a05d4b3ab8a58aa258e0f63300a59e6058b1e
Szymon Kwiatkowski <szymonkwiatkowski@onet.pl>
2022-09-17 18:32:18 +0200

    Added animations and compressed images

---
+++ app.js
@@ -0,0 +1,22 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } //else {
+    //   entry.target.classList.remove("show");
+    // }
+  });
+});
+
+const hiddenTop = document.querySelectorAll(".hidden-top");
+hiddenTop.forEach((el) => observer.observe(el));
+
+const hiddenBottom = document.querySelectorAll(".hidden-bottom");
+hiddenBottom.forEach((el) => observer.observe(el));
+
+const hiddenLeft = document.querySelectorAll(".hidden-left");
+hiddenLeft.forEach((el) => observer.observe(el));
+
+const hiddenRight = document.querySelectorAll(".hidden-right");
+hiddenRight.forEach((el) => observer.observe(el));
